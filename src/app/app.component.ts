db228560e2fafd8142a908bfcf2dc40f5e24f65d
ElenaShylionak <99590421+ElenaShylionak@users.noreply.github.com>
2022-09-25 20:41:53 +0300

    7 days intensive project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,120 @@
-import { Component } from '@angular/core';
+// @ts-ignore
+
+import {Component, HostListener} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'IntensiveFrontend2';
+  priceForm = this.fb.group({
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+    car: ['', Validators.required],
+  });
+
+  carsData = [
+    {
+      image: "1.png",
+      name: "Lamborghini Huracan Spyder",
+      box: "автомат",
+      engine: 5.2,
+      year: 2019
+
+    },
+    {
+      image: "2.png",
+      name: "Ferrari Californiar",
+      box: "автомат",
+      engine: 6.2,
+      year: 2017
+    },
+
+    {
+      image: "3.png",
+      name: "Chevrolet Corvette",
+      box: "автомат",
+      engine: 3.9,
+      year: 2010
+    },
+
+    {
+      image: "4.png",
+      name: "Lamborghini Urus",
+      box: "автомат",
+      engine: 4,
+      year: 2019
+    },
+
+    {
+      image: "5.png",
+      name: "Audi R8",
+      box: "автомат",
+      engine: 5.2,
+      year: 2018
+    },
+
+    {
+      image: "6.png",
+      name: "Chevrolet Camaro",
+      box: "автомат",
+      engine: 2,
+      year: 2019
+    },
+    {
+      image: "7.png",
+      name: "Maserati Quattroporte",
+      box: "автомат",
+      engine: 3,
+      year: 2018
+    },
+    {
+      image: "8.png",
+      name: "Dodge Sharepointe",
+      box: "автомат",
+      engine: 6.4,
+      year: 2019
+    },
+    {
+      image: "9.png",
+      name: "Nissan GT-R",
+      box: "механика",
+      engine: 3.8,
+      year: 2019
+    },
+
+  ]
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  goScroll(target: HTMLElement, car?: any) {
+    target.scrollIntoView({behavior: "smooth"});
+    if (car) {
+      this.priceForm.patchValue({car: car.name});
+    }
+  }
+
+  trans: any;
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    this.trans = {transform: 'translate3d(' + ((e.clientX * 0.3) / 8) + 'px,' + ((e.clientY * 0.3) / 8) + 'px,0px)'};
+  }
+
+  bgPos: any;
+  @HostListener('document:scroll', ['$event'])
+  onScroll() {
+    this.bgPos = {backgroundPositionX: '0' + (0.3 * window.scrollY) + 'px'};
+  }
+
+  onSubmit() {
+    if (this.priceForm.valid) {
+      alert("Спасибо. Mы свяжемся с вами!")
+      this.priceForm.reset();
+    }
+  }
 }
+
